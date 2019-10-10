a8f1fe981127b01d57f23a5216f23aa76e178d53
KouichiNakamura <kouichinakamura0610@gmail.com>
2019-10-11 03:17:35 +0900

    add comment

--- db/migrate/20191010181340_change_datatype_description_of_foods.rb
+++ db/migrate/20191010181340_change_datatype_description_of_foods.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeDescriptionOfFoods < ActiveRecord::Migration[5.2]
   def change
-    # この行は手動で追加 string -> text
+    # この行は空のマイグレーションを作成、手動で追加 string -> text
     change_column :foods, :description, :text, default: "食事の内容を記述"
   end
 end
