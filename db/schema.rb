1200f3f0a8949d879cd4e3b856d78f7e86f2389e
KouichiNakamura <kouichinakamura0610@gmail.com>
2019-10-11 02:48:44 +0900

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_134917) do
+ActiveRecord::Schema.define(version: 2019_10_10_145546) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
