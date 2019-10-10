7a509255e3d02aceb7d349c05879ac2200afd4d1
KouichiNakamura <kouichinakamura0610@gmail.com>
2019-10-11 03:25:56 +0900

    change email to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_181829) do
+ActiveRecord::Schema.define(version: 2019_10_10_182440) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_181829) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
