96b300d7730835bcbe9a2348a99a94c8e217ce85
KouichiNakamura <kouichinakamura0610@gmail.com>
2019-10-13 01:26:55 +0900

    add form object

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    # titleが空でなければabsence:trueを判定
+    validates :description, absence: true, unless: :title?
+    validates :description, length: { maximum: 100 }
+#    validates :description, format: { with: /\w/ }
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "山田太郎", address: "東京都港区", email: "ta@abc.jp")
+User.create(name: "田中花子", address: "東京都港区", email: "hk@abc.jp")
+User.create(name: "山崎", address: "東京都品川区", email: "tn@abc.jp")
+User.create(name: "佐々", address: "東京都品川区", email: "ic@abc.jp")
+User.create(name: "大友", address: "東京都港区", email: "to@abc.jp")
+User.create(name: "山田太郎", address: "北海道札幌市", email: "yt@abc.jp")
+
